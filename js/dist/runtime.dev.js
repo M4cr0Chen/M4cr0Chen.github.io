822fa0d66f8397ef087bffcb4bd4f595f334ee1e
Macro <macrochen05@gmail.com>
2024-11-07 17:36:11 -0500

    Site updated: 2024-11-07 17:35:40

--- js/dist/runtime.dev.js
+++ js/dist/runtime.dev.js
@@ -1,1 +1,34 @@
-"use strict";var now=new Date;function createtime(){now.setTime(now.getTime()+1e3);var t=new Date("08/01/2022 00:00:00"),e=Math.trunc(234e8+(now-t)/1e3*17),a=(e/1496e5).toFixed(6),n=new Date("08/09/2022 00:00:00"),o=(now-n)/1e3/60/60/24,c=Math.floor(o),r=(now-n)/1e3/60/60-24*c,i=Math.floor(r);1==String(i).length&&(i="0"+i);var s=(now-n)/1e3/60-1440*c-60*i,d=Math.floor(s);1==String(d).length&&(d="0"+d);var l=(now-n)/1e3-86400*c-3600*i-60*d,g=Math.round(l);1==String(g).length&&(g="0"+g);var b;b=i<18&&i>=9?"<img class='boardsign' src='https://sourcebucket.s3.ladydaily.com/badge/F小屋-科研摸鱼中.svg' title='什么时候能够实现财富自由呀~'><br> <div style=\"font-size:13px;font-weight:bold\">本站居然运行了 ".concat(c," 天 ").concat(i," 小时 ").concat(d," 分 ").concat(g," 秒 <i id=\"heartbeat\" class='fas fa-heartbeat'></i> <br> 旅行者 1 号当前距离地球 ").concat(e," 千米，约为 ").concat(a," 个天文单位 🚀</div>"):"<img class='boardsign' src='https://sourcebucket.s3.ladydaily.com/badge/F小屋-下班休息啦.svg' title='下班了就该开开心心地玩耍~'><br> <div style=\"font-size:13px;font-weight:bold\">本站居然运行了 ".concat(c," 天 ").concat(i," 小时 ").concat(d," 分 ").concat(g," 秒 <i id=\"heartbeat\" class='fas fa-heartbeat'></i> <br> 旅行者 1 号当前距离地球 ").concat(e," 千米，约为 ").concat(a," 个天文单位 🚀</div>"),document.getElementById("workboard")&&(document.getElementById("workboard").innerHTML=b)}setInterval((function(){createtime()}),1e3);
+"use strict";
+
+var now = new Date();
+
+function createtime() {
+  // 当前时间
+  now.setTime(now.getTime() + 1000);
+  var start = new Date("08/01/2022 00:00:00"); // 旅行者1号开始计算的时间
+
+  var dis = Math.trunc(23400000000 + (now - start) / 1000 * 17); // 距离=秒数*速度 记住转换毫秒
+
+  var unit = (dis / 149600000).toFixed(6); // 天文单位
+
+  var grt = new Date("08/09/2022 00:00:00"); // 网站诞生时间
+
+  var days = (now - grt) / 1e3 / 60 / 60 / 24,
+      dnum = Math.floor(days),
+      hours = (now - grt) / 1e3 / 60 / 60 - 24 * dnum,
+      hnum = Math.floor(hours);
+  1 == String(hnum).length && (hnum = "0" + hnum);
+  var minutes = (now - grt) / 1e3 / 60 - 1440 * dnum - 60 * hnum,
+      mnum = Math.floor(minutes);
+  1 == String(mnum).length && (mnum = "0" + mnum);
+  var seconds = (now - grt) / 1e3 - 86400 * dnum - 3600 * hnum - 60 * mnum,
+      snum = Math.round(seconds);
+  1 == String(snum).length && (snum = "0" + snum);
+  var currentTimeHtml = "";
+  currentTimeHtml = hnum < 18 && hnum >= 9 ? "<img class='boardsign' src='https://sourcebucket.s3.ladydaily.com/badge/F\u5C0F\u5C4B-\u79D1\u7814\u6478\u9C7C\u4E2D.svg' title='\u4EC0\u4E48\u65F6\u5019\u80FD\u591F\u5B9E\u73B0\u8D22\u5BCC\u81EA\u7531\u5440~'><br> <div style=\"font-size:13px;font-weight:bold\">\u672C\u7AD9\u5C45\u7136\u8FD0\u884C\u4E86 ".concat(dnum, " \u5929 ").concat(hnum, " \u5C0F\u65F6 ").concat(mnum, " \u5206 ").concat(snum, " \u79D2 <i id=\"heartbeat\" class='fas fa-heartbeat'></i> <br> \u65C5\u884C\u8005 1 \u53F7\u5F53\u524D\u8DDD\u79BB\u5730\u7403 ").concat(dis, " \u5343\u7C73\uFF0C\u7EA6\u4E3A ").concat(unit, " \u4E2A\u5929\u6587\u5355\u4F4D \uD83D\uDE80</div>") : "<img class='boardsign' src='https://sourcebucket.s3.ladydaily.com/badge/F\u5C0F\u5C4B-\u4E0B\u73ED\u4F11\u606F\u5566.svg' title='\u4E0B\u73ED\u4E86\u5C31\u8BE5\u5F00\u5F00\u5FC3\u5FC3\u5730\u73A9\u800D~'><br> <div style=\"font-size:13px;font-weight:bold\">\u672C\u7AD9\u5C45\u7136\u8FD0\u884C\u4E86 ".concat(dnum, " \u5929 ").concat(hnum, " \u5C0F\u65F6 ").concat(mnum, " \u5206 ").concat(snum, " \u79D2 <i id=\"heartbeat\" class='fas fa-heartbeat'></i> <br> \u65C5\u884C\u8005 1 \u53F7\u5F53\u524D\u8DDD\u79BB\u5730\u7403 ").concat(dis, " \u5343\u7C73\uFF0C\u7EA6\u4E3A ").concat(unit, " \u4E2A\u5929\u6587\u5355\u4F4D \uD83D\uDE80</div>"), document.getElementById("workboard") && (document.getElementById("workboard").innerHTML = currentTimeHtml);
+} // 设置重复执行函数，周期1000ms
+
+
+setInterval(function () {
+  createtime();
+}, 1000);
--- js/dist/snow.dev.js
+++ js/dist/snow.dev.js
@@ -1,1 +1,95 @@
-"use strict";navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)||window&&function(){var e=50,t=150,i="255, 255, 255",n=1.5,o=.5,a=.7,r=.5,d=window.requestAnimationFrame||window.mozRequestAnimationFrame||window.webkitRequestAnimationFrame||window.msRequestAnimationFrame||function(e){window.setTimeout(e,1e3/60)};window.requestAnimationFrame=d;var h=document.getElementById("snow"),s=h.getContext("2d"),w=e,l=-100,m=-100,v=[];h.width=window.innerWidth,h.height=window.innerHeight;var c=function e(){s.clearRect(0,0,h.width,h.height);for(var n=t,o=0;o<w;o++){var a=v[o],r=l,c=m,M=a.x,p=a.y,f=Math.sqrt((r-M)*(r-M)+(c-p)*(c-p));if(f<n){var g=(r-M)/f,y=(c-p)/f,Y=n/(f*f)/2;a.velX-=Y*g,a.velY-=Y*y}else a.velX*=.98,a.velY<a.speed&&a.speed-a.velY>.01&&(a.velY+=.01*(a.speed-a.velY)),a.velX+=Math.cos(a.step+=.05)*a.stepSize;s.fillStyle="rgba("+i+", "+a.opacity+")",a.y+=a.velY,a.x+=a.velX,(a.y>=h.height||a.y<=0)&&u(a),(a.x>=h.width||a.x<=0)&&u(a),s.beginPath(),s.arc(a.x,a.y,a.size,0,2*Math.PI),s.fill()}d(e)},u=function(e){e.x=Math.floor(Math.random()*h.width),e.y=0,e.size=3*Math.random()+2,e.speed=1*Math.random()+.5,e.velY=e.speed,e.velX=0,e.opacity=.5*Math.random()+.3};document.addEventListener("mousemove",(function(e){l=e.clientX,m=e.clientY})),window.addEventListener("resize",(function(){h.width=window.innerWidth,h.height=window.innerHeight})),function(){for(var e=0;e<w;e++){var t=Math.floor(Math.random()*h.width),i=Math.floor(Math.random()*h.height),d=3*Math.random()+n,s=1*Math.random()+o,l=.5*Math.random()+a;v.push({speed:s,velX:0,velY:s,x:t,y:i,size:d,stepSize:Math.random()/30*r,step:0,angle:180,opacity:l})}c()}()}();
+"use strict";
+
+if (navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)) {// 移动端不显示
+} else {
+  // document.write('<canvas id="snow" style="position:fixed;top:0;left:0;width:100%;height:100%;z-index:-2;pointer-events:none"></canvas>');
+  window && function () {
+    var e = {
+      flakeCount: 50,
+      // 雪花数目
+      minDist: 150,
+      // 最小距离
+      color: "255, 255, 255",
+      // 雪花颜色
+      size: 1.5,
+      // 雪花大小
+      speed: .5,
+      // 雪花速度
+      opacity: .7,
+      // 雪花透明度
+      stepsize: .5 // 步距
+
+    };
+
+    var t = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function (e) {
+      window.setTimeout(e, 1e3 / 60);
+    };
+
+    window.requestAnimationFrame = t;
+    var i = document.getElementById("snow"),
+        n = i.getContext("2d"),
+        o = e.flakeCount;
+    var a = -100,
+        d = -100,
+        s = [];
+    i.width = window.innerWidth, i.height = window.innerHeight;
+
+    var h = function h() {
+      n.clearRect(0, 0, i.width, i.height);
+      var r = e.minDist;
+
+      for (var _t = 0; _t < o; _t++) {
+        var _o = s[_t];
+        var _h = a,
+            w = d,
+            m = _o.x,
+            c = _o.y,
+            p = Math.sqrt((_h - m) * (_h - m) + (w - c) * (w - c));
+
+        if (p < r) {
+          var _e = (_h - m) / p,
+              _t2 = (w - c) / p,
+              _i = r / (p * p) / 2;
+
+          _o.velX -= _i * _e, _o.velY -= _i * _t2;
+        } else _o.velX *= .98, _o.velY < _o.speed && _o.speed - _o.velY > .01 && (_o.velY += .01 * (_o.speed - _o.velY)), _o.velX += Math.cos(_o.step += .05) * _o.stepSize;
+
+        n.fillStyle = "rgba(" + e.color + ", " + _o.opacity + ")", _o.y += _o.velY, _o.x += _o.velX, (_o.y >= i.height || _o.y <= 0) && l(_o), (_o.x >= i.width || _o.x <= 0) && l(_o), n.beginPath(), n.arc(_o.x, _o.y, _o.size, 0, 2 * Math.PI), n.fill();
+      }
+
+      t(h);
+    },
+        l = function l(e) {
+      e.x = Math.floor(Math.random() * i.width), e.y = 0, e.size = 3 * Math.random() + 2, e.speed = 1 * Math.random() + .5, e.velY = e.speed, e.velX = 0, e.opacity = .5 * Math.random() + .3;
+    };
+
+    document.addEventListener("mousemove", function (e) {
+      a = e.clientX, d = e.clientY;
+    }), window.addEventListener("resize", function () {
+      i.width = window.innerWidth, i.height = window.innerHeight;
+    }), function () {
+      for (var _t3 = 0; _t3 < o; _t3++) {
+        var _t4 = Math.floor(Math.random() * i.width),
+            _n = Math.floor(Math.random() * i.height),
+            _o2 = 3 * Math.random() + e.size,
+            _a = 1 * Math.random() + e.speed,
+            _d = .5 * Math.random() + e.opacity;
+
+        s.push({
+          speed: _a,
+          velX: 0,
+          velY: _a,
+          x: _t4,
+          y: _n,
+          size: _o2,
+          stepSize: Math.random() / 30 * e.stepsize,
+          step: 0,
+          angle: 180,
+          opacity: _d
+        });
+      }
+
+      h();
+    }();
+  }();
+}
